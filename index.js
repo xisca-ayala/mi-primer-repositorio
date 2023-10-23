114e7e8f00d31375716e7403ef1dce3aab00aefc
xisca-ayala <xisca.ayala@iMac-de-Antoni.bbrouter>
2023-10-23 19:16:42 +0200

    add operaciones

---
+++ index.js
@@ -0,0 +1,22 @@
+function suma(x,y){
+    return x+y;
+};
+
+function resta(x,y){
+    return x-y;
+};
+
+function producto(x,y){
+    return x*y;
+};
+
+function division(x,y){
+    return x/y;
+};
+
+function cuadrado(x){
+    return x**2;
+}
+
+module.exports={suma, resta, producto, division, cuadrado};
+
---
+++ run.js
@@ -0,0 +1,15 @@
+let myLib = require("./index");
+
+
+let resultSuma = myLib.suma(2,3);
+let resultResta = myLib.resta(2,3);
+let resultProducto = myLib.producto(2,3);
+let resultDivision = myLib.division(2,3);
+let resultCuadrado = myLib.cuadrado(3);
+
+console.log("El resultado de la suma es: " + resultSuma);
+console.log("El resultado de la resta es: " + resultResta);
+console.log("El resultado de la multiplicación es: " + resultProducto);
+console.log("El resultado de la división es: " + resultDivision);
+console.log("El resultado del cuadrado es: " + resultCuadrado);
+ 
